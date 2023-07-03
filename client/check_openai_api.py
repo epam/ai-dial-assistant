3d713adf4c48b6720d365b7c8136c9d214746008
Alexey Klimov <alexey_klimov@epam.com>
2023-07-03 20:55:17 +0100

    Move methods back to a single file.

--- client/check_openai_api.py
+++ client/check_openai_api.py
@@ -4,7 +4,48 @@ import typing
 
 import openai
 
-from client.utils import merge
+
+def merge_str(target, source):
+    if target is None:
+        return source
+    else:
+        return target + source
+
+
+def merge_dicts(target, source):
+    for key, value in source.items():
+        if key in target:
+            if isinstance(value, int):
+                target[key] = value
+            elif isinstance(value, str):
+                target[key] = merge_str(target[key], value)
+            else:
+                merge(target[key], value)
+        else:
+            target[key] = value
+
+    return target
+
+
+def merge_lists(target, source):
+    for i in source:
+        index = i['index']
+
+        if index < len(target):
+            merge(target[index], i)
+        else:
+            target.append(i)
+
+    return target
+
+
+def merge(target, source):
+    if isinstance(source, list):
+        return merge_lists(target, source)
+
+    if isinstance(source, dict):
+        return merge_dicts(target, source)
+
 
 openai.api_key = os.environ["RAIL_PROXY_API_KEY"]
 openai.api_type = "azure"
