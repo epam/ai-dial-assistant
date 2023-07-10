a259cd2e3bdd6a1407f91eb6cdec2e18dfd136eb
Alexey Klimov <alexey_klimov@epam.com>
2023-07-10 15:50:43 +0100

    Update token header.

--- utils/addon_token_source.py
+++ utils/addon_token_source.py
@@ -5,7 +5,7 @@ from starlette.datastructures import Headers
 class AddonTokenSource:
     def __init__(self, headers: Headers, urls: list[str]):
         self.headers = headers
-        self.urls = {url: f"addon-{index}" for index, url in enumerate(urls)}
+        self.urls = {url: f"addon-token-{index}" for index, url in enumerate(urls)}
 
     def get_token(self, url: str) -> str | None:
         return self.headers.get(self.urls[url])
