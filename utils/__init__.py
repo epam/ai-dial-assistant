aa1662263e1ecbfcd0a37ab7316702e288b6291d
Alexey Klimov <alexey_klimov@epam.com>
2023-09-12 13:26:23 +0100

    Fix dependencies.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,4 @@
-import logging
 import logging.config
-
-from dotenv import load_dotenv
 
 logging.config.fileConfig("logging.conf")
 logger = logging.getLogger(__name__)
-load_dotenv()  # take environment variables from .env.
