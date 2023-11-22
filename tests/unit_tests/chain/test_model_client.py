9542ac9b924d110f7582119e71f0daac5dfd06a0
Alexey Klimov <alexey_klimov@epam.com>
2023-11-22 16:53:45 +0000

    Add more model client tests.

--- tests/unit_tests/chain/test_model_client.py
+++ tests/unit_tests/chain/test_model_client.py
@@ -5,7 +5,9 @@ import pytest
 
 from aidial_assistant.chain.model_client import (
     ExtraResultsCallback,
+    Message,
     ModelClient,
+    ReasonLengthException,
 )
 from aidial_assistant.utils.text import join_string
 from tests.utils.async_helper import to_async_list
@@ -33,4 +35,71 @@ async def test_discarded_messages(api):
 
     assert extra_results_callback.set_discarded_messages.call_args_list == [
         call(2)
+    ]
+
+
+@mock.patch(API_METHOD)
+@pytest.mark.asyncio
+async def test_content(api):
+    model_client = ModelClient(MODEL_ARGS, BUFFER_SIZE)
+    api.return_value = to_async_list(
+        [
+            {"choices": [{"delta": {"content": "one, "}}]},
+            {"choices": [{"delta": {"content": "two, "}}]},
+            {"choices": [{"delta": {"content": "three"}}]},
+        ]
+    )
+
+    assert await join_string(model_client.agenerate([])) == "one, two, three"
+
+
+@mock.patch(API_METHOD)
+@pytest.mark.asyncio
+async def test_reason_length_with_usage(api):
+    model_client = ModelClient(MODEL_ARGS, BUFFER_SIZE)
+    api.return_value = to_async_list(
+        [
+            {"choices": [{"delta": {"content": "text"}}]},
+            {
+                "choices": [
+                    {"delta": {"content": ""}, "finish_reason": "length"}  # type: ignore
+                ]
+            },
+            {
+                "choices": [{"delta": {"content": ""}}],
+                "usage": {"prompt_tokens": 1, "completion_tokens": 2},
+            },
+        ]
+    )
+
+    with pytest.raises(ReasonLengthException):
+        async for chunk in model_client.agenerate([]):
+            assert chunk == "text"
+
+    assert model_client.prompt_tokens == 1
+    assert model_client.completion_tokens == 2
+
+
+@mock.patch(API_METHOD)
+@pytest.mark.asyncio
+async def test_api_args(api):
+    model_client = ModelClient(MODEL_ARGS, BUFFER_SIZE)
+    api.return_value = to_async_list([])
+    messages = [
+        Message.system(content="a"),
+        Message.user(content="b"),
+        Message.assistant(content="c"),
+    ]
+
+    await join_string(model_client.agenerate(messages))
+
+    assert api.call_args_list == [
+        call(
+            messages=[
+                {"role": "system", "content": "a"},
+                {"role": "user", "content": "b"},
+                {"role": "assistant", "content": "c"},
+            ],
+            **MODEL_ARGS,
+        )
     ]
