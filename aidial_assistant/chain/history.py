faea324c6f2c70569cf1dc7705a8ecba7fc7a97e
Oleksii-Klimov <133792808+Oleksii-Klimov@users.noreply.github.com>
2023-11-28 15:18:49 +0000

    Fix history formatting. (#34)

--- aidial_assistant/chain/history.py
+++ aidial_assistant/chain/history.py
@@ -1,10 +1,13 @@
-import json
 from enum import Enum
 
 from aidial_sdk.chat_completion import Role
 from jinja2 import Template
 from pydantic import BaseModel
 
+from aidial_assistant.chain.command_result import (
+    CommandInvocation,
+    commands_to_text,
+)
 from aidial_assistant.chain.dialogue import Dialogue
 from aidial_assistant.chain.model_client import Message
 from aidial_assistant.commands.reply import Reply
@@ -54,13 +57,12 @@ class History:
 
             elif scope == MessageScope.USER and message.role == Role.ASSISTANT:
                 # Clients see replies in plain text, but the model should understand how to reply appropriately.
-                content = json.dumps(
-                    {
-                        "commands": {
-                            "command": Reply.token(),
-                            "args": [message.content],
-                        }
-                    }
+                content = commands_to_text(
+                    [
+                        CommandInvocation(
+                            command=Reply.token(), args=[message.content]
+                        )
+                    ]
                 )
                 messages.append(Message.assistant(content=content))
             else:
