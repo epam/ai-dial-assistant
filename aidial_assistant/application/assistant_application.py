183310b1b75db7c129bf884fc32abcda853fcf2b
Oleksii-Klimov <133792808+Oleksii-Klimov@users.noreply.github.com>
2023-12-14 18:21:44 +0000

    feat: do not propagate jwt to model (#43)
    Do not propagate jwt to model.

--- aidial_assistant/application/assistant_application.py
+++ aidial_assistant/application/assistant_application.py
@@ -5,7 +5,6 @@ from aidial_sdk.chat_completion import FinishReason
 from aidial_sdk.chat_completion.base import ChatCompletion
 from aidial_sdk.chat_completion.request import Addon, Message, Request, Role
 from aidial_sdk.chat_completion.response import Response
-from aiohttp import hdrs
 
 from aidial_assistant.application.addons_dialogue_limiter import (
     AddonsDialogueLimiter,
@@ -47,9 +46,6 @@ def _get_request_args(request: Request) -> dict[str, str]:
         "api_version": request.api_version,
         "api_key": request.api_key,
         "user": request.user,
-        "headers": None
-        if request.jwt is None
-        else {hdrs.AUTHORIZATION: request.jwt},
     }
 
     return {k: v for k, v in args.items() if v is not None}
