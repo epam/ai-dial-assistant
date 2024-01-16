446c7069dfd0acb6f58d1dc5725d4e56bf7f02b8
Alexey Klimov <alexey_klimov@epam.com>
2024-01-16 13:39:37 +0000

    Update dial sdk to use fixed field name for otlp flag.

--- aidial_assistant/app.py
+++ aidial_assistant/app.py
@@ -8,17 +8,20 @@ from aidial_sdk.telemetry.types import TelemetryConfig, TracingConfig
 from aidial_assistant.utils.log_config import get_log_config
 
 log_level = os.getenv("LOG_LEVEL", "INFO")
-
-logging.config.dictConfig(get_log_config(log_level))
-
 otlp_export_enabled: bool = (
     os.environ.get("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT") is not None
 )
-telemetry_config = TelemetryConfig(
-    service_name="aidial-assistant",
-    tracing=TracingConfig(oltp_export=otlp_export_enabled, logging=True),
+config_dir = Path(os.getenv("CONFIG_DIR", "aidial_assistant/configs"))
+tools_supporting_deployments: set[str] = set(
+    os.getenv("TOOLS_SUPPORTING_DEPLOYMENTS", "").split(",")
 )
 
+logging.config.dictConfig(get_log_config(log_level))
+
+telemetry_config = TelemetryConfig(
+    service_name="aidial-assistant",
+    tracing=TracingConfig(otlp_export=otlp_export_enabled, logging=True),
+)
 app = DIALApp(telemetry_config=telemetry_config)
 
 # A delayed import is necessary to set up the httpx hook before the openai client inherits from AsyncClient.
@@ -26,10 +29,6 @@ from aidial_assistant.application.assistant_application import (  # noqa: E402
     AssistantApplication,
 )
 
-config_dir = Path(os.getenv("CONFIG_DIR", "aidial_assistant/configs"))
-tools_supporting_deployments: set[str] = set(
-    os.getenv("TOOLS_SUPPORTING_DEPLOYMENTS", "").split(",")
-)
 app.add_chat_completion(
     "assistant",
     AssistantApplication(config_dir, tools_supporting_deployments),
